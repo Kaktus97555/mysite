de395c3d6d0a854f41a2c11ef5681ebab7000133
Kaktus97555 <23also975@gmail.com>
2026-07-02 17:35:58 +0200

    Update getFiles.js

--- netlify/functions/getFiles.js
+++ netlify/functions/getFiles.js
@@ -35,38 +35,4 @@ exports.handler = async (event) => {
       body: JSON.stringify({ error: error.message }),
     };
   }
-};const { google } = require('googleapis');
-
-exports.handler = async (event) => {
-  try {
-    const auth = new google.auth.JWT(
-      process.env.GOOGLE_CLIENT_EMAIL,
-      null,
-      process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      ['https://www.googleapis.com/auth/drive.readonly']
-    );
-
-    const drive = google.drive({ version: 'v3', auth });
-
-    // Pobieramy ID folderu z zapytania strony. Jeśli go nie ma, używamy głównego z ustawień Netlify.
-    const folderId = event.queryStringParameters.folderId || process.env.DRIVE_FOLDER_ID;
-
-    // Pobieramy pliki ORAZ foldery
-    const response = await drive.files.list({
-      q: `'${folderId}' in parents and trashed=false`,
-      // Pobieramy format pliku (mimeType), miniaturkę i linki
-      fields: 'files(id, name, mimeType, webViewLink, thumbnailLink)', 
-      orderBy: 'folder, name' // Najpierw foldery, potem pliki, alfabetycznie
-    });
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify(response.data.files),
-    };
-  } catch (error) {
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: error.message }),
-    };
-  }
 };
